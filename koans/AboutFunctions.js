8c1a8a0ea28e698c68ed7a6007c6fcb2e911d57c
Bryan Lee <pygmize.me@gmail.com>
2015-02-24 07:37:28 -0800

    Fixed AboutFunctions.js "funtion body as string"

--- koans/AboutFunctions.js
+++ koans/AboutFunctions.js
@@ -104,9 +104,9 @@ describe("About Functions", function() {
       return a * b;
     };
 
-    expect(multiply.toString()).toBe("function (a, b) {\n" + 
-    "      //An internal comment\n" + 
-    "      return a * b;\n" + 
+    expect(multiply.toString()).toBe("function (a, b) {\r\n" + 
+    "      //An internal comment\r\n" + 
+    "      return a * b;\r\n" + 
     "    }");
   }); 
 });
